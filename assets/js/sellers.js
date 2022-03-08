207a80b4cb51575c40b8270931ebd8fe044afe0c
Alireza <alireza27163@gmail.com>
2022-03-08 16:24:48 +0200

    Second day

---
+++ assets/js/sellers.js
@@ -0,0 +1,28 @@
+const passwordInput = document.querySelector("#salasana");
+const generatePassword = document.querySelector("#generatePassword");
+const characters = ["A","B","C","D","E","F","G","H","K","M","N","P","R","S","T","U","W","X","Y","Z","a","b","c","d","e","f","g","h","k","m","n","p","r","s","t","u","v","w","x","y","z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "<", ">"];
+
+const generatePasswordFn = () => {
+    let password = [];
+    for(let i = 0; i < 8; i++){
+        let item = characters[Math.floor(Math.random() * characters.length)];
+        if(!password.includes(item)){
+            password.push(item)
+        }else{
+            let item = characters[Math.floor(Math.random() * characters.length)];
+            password.push(item)
+
+        }
+    }
+    word = password.join("");
+    return word
+}
+
+
+generatePassword.addEventListener("click", () => {
+    passwordInput.value = generatePasswordFn()
+})
+
+
+
+
